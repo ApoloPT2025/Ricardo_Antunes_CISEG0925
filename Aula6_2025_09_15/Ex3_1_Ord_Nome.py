9ef7eb82eb0e2004c258c633c89e7f6396314a6e
ApoloPT2025 <cokrecursos6@gmail.com>
2025-09-15 22:33:23 +0100

    sync 2025_09_15

--- Aula6_2025_09_15/Ex3_1_Ord_Nome.py
+++ Aula6_2025_09_15/Ex3_1_Ord_Nome.py
@@ -1,5 +1,5 @@
-#listaNome=["Ricardo Antunes","Ana Santos","Sonia Carina","Maria Russ","Ana Silva","Margarida Marques","Andreia Torres","Ana Antunes","Renato Gaspar","Andreia Alves"]
-listaNome=["Dario Quental","Dario Almeida","Bruno Carvalho"]
+listaNome=["Ricardo Antunes","Ana Santos","Sonia Carina","Antonio Magro","Maria Russ","Ana Silva","Margarida Marques","Andreia Torres","Ana Antunes","Renato Gaspar","Andreia Alves"]
+#listaNome=["Dario Quental","Dario Almeida","Bruno Carvalho"]
 # index         0               1                 2
 #length 6
  
@@ -18,38 +18,42 @@ while il<len(listaNome): #Loop controla as voltas a lista
     #print("Ordenado=",ordenado)
     #input()
     while i<len(listaNome) : #Loop controla a posiçao do nome
-        #print("Entrou dentro do loop I , pos=",i,"/",len(listaNome))
+        print("Entrou dentro do loop I , pos=",i,"/",len(listaNome))
         #input()
         if i<=len(listaNome)-2:
             controloTroca=-1 #Reposiçao da variavel troca // tambem seria possivel usar um break
             it=0  #Reposiçao do index da lista 2 dimensão ( Letras )
             while it<len(listaNome[i]) and it<len(listaNome[i+1]): #Loop controla as letras no nome
-                #print("Entrou dentro do loop IT , pos=",it,"/",len(listaNome[i]))
-                #print(listaNome[i][it]," - ",listaNome[i+1][it])
+                print("Entrou dentro do loop IT , pos=",it,"/",len(listaNome[i]))
+                print(listaNome[i][it]," - ",listaNome[i+1][it])
                 #input()
-                if ord(listaNome[i][it])>ord(listaNome[i+1][it]) and controloTroca!=i : #Se letra > a proxima
-                    #print("I=",i,"I+1=",i+1)
-                    #print("Entrou dentro do if letra > proxima")
-                    #print(listaNome[i][it]," - ",listaNome[i+1][it]) #Print da letra com a proxima
-                    #print(listaNome) #Print da lista antes da troca
+                if ord(listaNome[i][it])>ord(listaNome[i+1][it]) and controloTroca!=i: #Se letra > a proxima
+                    print("I=",i,"I+1=",i+1)
+                    print("Entrou dentro do if letra > proxima")
+                    print(listaNome[i][it]," - ",listaNome[i+1][it]) #Print da letra com a proxima
+                    print(listaNome) #Print da lista antes da troca
                     controloTroca=i #Controlar index da primeira dimençao ( quando acontece troca de nome ja nao é trocado nenhum nome)
                     listaNome.insert(i,listaNome[i+1])
                     #print(listaNome) #Print da lista quando muda o nome de lugar
                     listaNome.pop(i+2)
-                    #print(listaNome) #Print da lista depois da troca
+                    print(listaNome) #Print da lista depois da troca
                     #ordenado+=1
                     #print("Ordenado=",ordenado)
                     #it=0
                     #input()
                     break
+                if ord(listaNome[i][it])<ord(listaNome[i+1][it]) and controloTroca!=i:
+                    print("Inferior")
+                    #input()
+                    break
                 #else:
                 it+=1
-                break
-            #print("Saiu do loop IT")
+                #break
+            print("Saiu do loop IT")
             #input()
         i+=1
-    #print("Saiu do loop I")
-    #input()
+        print("Saiu do loop I")
+        #input()
     il+=1
 print(listaNome)
 print("Fim")
